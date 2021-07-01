44fc45925a320e002b3c1f9b906656336a8fb75d
Chitoku YATO <chitoku@gmail.com>
2021-06-30 17:28:56 -0700

    Fix 'disconnected' nic issue

--- edit_autostart.sh
+++ edit_autostart.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ACTIVE_NIC=$(nmcli device status | grep -e "connected" | grep -v "docker" | awk '{print $1}')
+active_nic=$(nmcli device status | grep -e "connected" | grep -v "disconnected" | grep -v "docker" | awk 'NR==1{print $1}')
 
-echo ${ACTIVE_NIC}
+echo "active_nic: ${active_nic}"
 
 cat ~/.config/autostart/indicator-ip.desktop | grep "Exec"
 
-NEW_COMMAND="Exec=bash -c \"sleep 30 ; indicator-ip --interface ${ACTIVE_NIC}\""
-echo $NEW_COMMAND
+new_command="Exec=bash -c \"sleep 30 ; indicator-ip --interface ${active_nic}\""
+echo $new_command
 
-sed -i "s|Exec=.*|$NEW_COMMAND|" ~/.config/autostart/indicator-ip.desktop
+sed -i "s|Exec=.*|$new_command|" ~/.config/autostart/indicator-ip.desktop
 
 cat ~/.config/autostart/indicator-ip.desktop | grep "Exec"
